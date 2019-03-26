f18b60f9c35c0761a73a88a5f66062beb1b9583e
Kim WangTJ <kim.wangtj@homecreditcfc.cn>
2019-03-26 18:39:08 +0800

    re-factory retrofit.

--- common/src/main/java/wq/android/mvvm/java/common/network/retrofit/RetrofitService.java
+++ common/src/main/java/wq/android/mvvm/java/common/network/retrofit/RetrofitService.java
@@ -14,22 +14,12 @@ import lombok.Getter;
 @Builder(builderClassName = "Builder")
 @Getter
 public class RetrofitService {
-
-    private static final Map<Class<?>, Object> mServiceCache = new ArrayMap<>(0);
-
-    public interface ResponseInterceptor {
-        /**
-         * interceptor for retrofit response.
-         *
-         * @param retrofitResponse
-         */
-        void onResponse(RetrofitResponse<?> retrofitResponse);
-    }
-
     private Class<?> serviceClass;
     private String basUrl;
     @lombok.Builder.Default
     private ResponseInterceptor responseInterceptor = new DefaultRetrofitResponseInterceptor();
+
+    private static final Map<Class<?>, Object> mServiceCache = new ArrayMap<>(0);
 
     public static <T> T get(RetrofitService.Builder builder) {
         RetrofitService retrofitService = builder.build();
@@ -40,5 +30,14 @@ public class RetrofitService {
             mServiceCache.put(serviceClass, service);
         }
         return (T) service;
+    }
+
+    public interface ResponseInterceptor {
+        /**
+         * interceptor for retrofit response.
+         *
+         * @param retrofitResponse
+         */
+        void onResponse(RetrofitResponse<?> retrofitResponse);
     }
 }
